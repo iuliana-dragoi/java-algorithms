7d8232029b20955e4a109176b3f91b2ac514e965
iuliana.dragoi <iulianadragoi95@gmail.com>
2026-01-13 19:36:04 +0200

    Implement RemoveDuplicates

---
+++ src/com/iuliana/algorithms/arrays/RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package com.iuliana.algorithms.arrays;
+
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+
+    public static String removeDuplicates(String input) {
+        HashSet<Character> exists = new HashSet<>();
+        StringBuilder result = new StringBuilder();
+        for(char c : input.toCharArray()) {
+            if(!exists.contains(c)) {
+                result.append(c);
+                exists.add(c);
+            }
+        }
+
+        return result.toString();
+    }
+
+    static void main() {
+        String input = "geeksforgeeks";
+        System.out.println(removeDuplicates(input));
+    }
+}
