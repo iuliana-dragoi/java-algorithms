1c40753c4cad43b4b0d001fe16d884e65bf9775f
iuliana.dragoi <iulianadragoi95@gmail.com>
2026-01-14 10:56:36 +0200

    Implement FizzBuzz

---
+++ src/com/iuliana/algorithms/arrays/FizzBuzz.java
@@ -0,0 +1,32 @@
+package com.iuliana.algorithms.arrays;
+
+public class FizzBuzz {
+
+    class Result {
+
+        public static void fizzBuzz(int n) {
+            int ct = 1;
+            while(n > 0) {
+
+                if(ct % 3 == 0 && ct % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                }
+                else if(ct % 3 == 0) {
+                    System.out.println("Fizz");
+                }
+                else if(ct % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+                else System.out.println(ct);
+
+                ct++;
+                n--;
+            }
+        }
+    }
+
+    static void main(String[] args) {
+        int n = 15;
+        Result.fizzBuzz(n);
+    }
+}
